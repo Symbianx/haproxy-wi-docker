1cf0775d604a1099d3c7453bd45af83dc3583018
Aidaho12 <buffoon2001@mail.ru>
2018-08-13 10:03:15 +0600

    0.1

---
+++ wrapper.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+/usr/sbin/httpd -DFOREGROUND
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start Apache: $status"
+  exit $status
+fi
+
+systemctl start metrics_haproxy.service 
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start Metrics HAProxy: $status"
+  exit $status
+fi
+
+systemctl start chcker_haproxy.service 
+status=$?
+if [ $status -ne 0 ]; then
+  echo "Failed to start Chcker HAProxy: $status"
+  exit $status
+fi
+
+while sleep 60; do
+  ps aux |grep /usr/sbin/httpd |grep -q -v grep
+  PROCESS_1_STATUS=$?
+  ps aux |grep metrics_master.py |grep -q -v grep
+  PROCESS_2_STATUS=$?
+  ps aux |grep checker_master.py.py |grep -q -v grep
+  PROCESS_3_STATUS=$?
+  
+  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 -o $PROCESS_3_STATUS -ne 0 ]; then
+    echo "One of the processes has already exited."
+    exit 1
+  fi
+done
+
